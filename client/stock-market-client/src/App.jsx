2aa39f79cd6d2399b2a8569ef5b81b0cf9f598b4
md-asik-reza-tasin <mdasikreza1997@gmail.com>
2024-12-21 23:03:25 +0600

    POST DATA

--- client/stock-market-client/src/App.jsx
+++ client/stock-market-client/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import { useEffect } from "react";
+import { MdAddCircleOutline } from "react-icons/md";
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
   const [stockData, setStockData] = useState([]);
   const [code, setCode] = useState("SELECT TRADE CODE");
+  const [postData, setPostData] = useState(null);
 
   useEffect(() => {
     fetch("http://127.0.0.1:5000/api/data")
@@ -15,7 +18,7 @@ function App() {
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [postData]);
 
   const tradeCode = [
     ...new Set(stockData.map((singleData) => singleData.trade_code)),
@@ -24,10 +27,52 @@ function App() {
   const stockInfo = stockData.filter((data) => data.trade_code === code);
   // console.log(stockInfo);
 
+  const handleNewStockData = (e) => {
+    e.preventDefault();
+
+    const target = e.target;
+    const modal = document.getElementById("my_modal_4");
+
+    const date = target.date.value;
+    const tradeCode = target.tradecode.value;
+    const high = target.high.value;
+    const low = target.low.value;
+    const open = target.open.value;
+    const close = target.close.value;
+    const volume = target.volume.value;
+
+    const newStock = {
+      date,
+      tradeCode,
+      high,
+      low,
+      low,
+      open,
+      close,
+      volume,
+    };
+
+    fetch("http://127.0.0.1:5000/api/data", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newStock),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message && modal) {
+          toast.success(data.message);
+          setPostData(data);
+          modal.close();
+        }
+      });
+  };
+
   return (
     <div className="w-screen h-screen flex flex-col items-center">
       <select
-        className="select select-bordered w-full max-w-xs mt-10"
+        className="select select-bordered mt-10"
         onChange={(e) => setCode(e.target.value)}
       >
         <option selected>SELECT TRADE CODE</option>
@@ -35,7 +80,80 @@ function App() {
           <option key={idx}>{trade}</option>
         ))}
       </select>
-      <div className="w-full h-full px-10 mt-10">
+      <div className="mt-10">
+        <button
+          className="flex justify-center items-center gap-1"
+          onClick={() => document.getElementById("my_modal_4").showModal()}
+        >
+          <MdAddCircleOutline className="size-4" />{" "}
+          <p className="text-sm">ADD STOCK</p>
+        </button>
+      </div>
+      <div>
+        <Toaster />
+      </div>
+      <dialog id="my_modal_4" className="modal">
+        <div className="modal-box p-16 w-11/12 max-w-5xl rounded-sm">
+          <form
+            onSubmit={handleNewStockData}
+            className="grid grid-cols-2 gap-5"
+          >
+            <input
+              type="text"
+              placeholder="YYYY-MM-DD"
+              className="input input-bordered input-md w-full"
+              name="date"
+            />
+            <input
+              type="text"
+              placeholder="trade_code"
+              className="input input-bordered input-md w-full "
+              name="tradecode"
+            />
+            <input
+              type="number"
+              placeholder="high"
+              className="input input-bordered input-md w-full "
+              name="high"
+            />
+            <input
+              type="number"
+              placeholder="low"
+              className="input input-bordered input-md w-full "
+              name="low"
+            />
+            <input
+              type="number"
+              placeholder="open"
+              className="input input-bordered input-md w-full "
+              name="open"
+            />
+            <input
+              type="number"
+              placeholder="close"
+              className="input input-bordered input-md w-full "
+              name="close"
+            />
+            <input
+              type="number"
+              placeholder="volume"
+              className="input input-bordered input-md w-full col-span-2"
+              name="volume"
+            />
+            <input
+              type="submit"
+              value="Submit"
+              className="bg-black opacity-80 w-full col-span-2 text-white h-12 rounded-md"
+            />
+          </form>
+          <div className="modal-action">
+            <form method="dialog">
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+      <div className="w-full h-full px-10 mt-5">
         <div className="overflow-x-auto">
           <table className="table table-lg">
             <thead className="bg-black opacity-70 text-white">
@@ -53,7 +171,7 @@ function App() {
 
             <tbody>
               {stockInfo?.map((codeInfo, idx) => (
-                <tr>
+                <tr key={idx}>
                   <td>{idx + 1}</td>
                   <td>{codeInfo.date}</td>
                   <td>{codeInfo.trade_code}</td>
