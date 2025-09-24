0ffcd2e1326adfc585da3a09c0dd50f65481aefb
md-asik-reza-tasin <mdasikreza1997@gmail.com>
2025-09-24 13:45:54 +0600

    FINAL UPDATE

--- client/stock-market-client/vite.config.js
+++ client/stock-market-client/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0", // Allow connections from outside the container
+    port: 5173       // Make sure it matches your docker run -p
+  }
 })
--- server_django/api/admin.py
+++ server_django/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import StockData
 
-# Register your models here.
+@admin.register(StockData)
+class StockDataAdmin(admin.ModelAdmin):
+    list_display = ('date', 'trade_code', 'high', 'low', 'open', 'close', 'volume')
+    search_fields = ('trade_code', 'date')
+    list_filter = ('trade_code',)
+    ordering = ('-date',)
