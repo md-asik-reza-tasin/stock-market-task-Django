c15c36502877955604169d9911db773fc95c47bf
md-asik-reza-tasin <mdasikreza1997@gmail.com>
2025-09-24 19:41:21 +0600

    BASE URL

--- client/stock-market-client/src/base_url.js
+++ client/stock-market-client/src/base_url.js
@@ -1,2 +1,4 @@
 // export const API_BASE_URL = 'http://127.0.0.1:8000/'; 
-export const API_BASE_URL = 'http://stock-market-backend-l3m8.onrender.com/'; // Change this to your actual API URL
+
+
+export const API_BASE_URL = 'https://stock-market-backend-l3m8.onrender.com/'; // Change this to your actual API URL
